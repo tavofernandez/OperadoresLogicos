9b6df4c58f333f2f74357dbd5388acb43dca0050
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-20 23:03:00 -0500

    Cambiamos el nombre de los principales controles para su mejor entendimiento

--- frmOperadoresLogicos.Designer.cs
+++ frmOperadoresLogicos.Designer.cs
@@ -33,13 +33,13 @@ namespace OperadoresLogicos
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnY = new System.Windows.Forms.Button();
+            this.btnO = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -79,65 +79,65 @@ namespace OperadoresLogicos
             this.label4.TabIndex = 2;
             this.label4.Text = "Resultado:";
             // 
-            // label5
+            // lblresultado
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(255, 333);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 13);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "---";
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(255, 333);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(16, 13);
+            this.lblresultado.TabIndex = 3;
+            this.lblresultado.Text = "---";
             // 
-            // textBox1
+            // txtnumero1
             // 
-            this.textBox1.Location = new System.Drawing.Point(264, 113);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(172, 20);
-            this.textBox1.TabIndex = 4;
+            this.txtnumero1.Location = new System.Drawing.Point(264, 113);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(172, 20);
+            this.txtnumero1.TabIndex = 4;
             // 
-            // textBox2
+            // txtnumero2
             // 
-            this.textBox2.Location = new System.Drawing.Point(264, 153);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(172, 20);
-            this.textBox2.TabIndex = 4;
+            this.txtnumero2.Location = new System.Drawing.Point(264, 153);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(172, 20);
+            this.txtnumero2.TabIndex = 4;
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.btnY);
+            this.flowLayoutPanel1.Controls.Add(this.btnO);
+            this.flowLayoutPanel1.Controls.Add(this.btnNo);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(255, 199);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(304, 90);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
-            // button1
+            // btnY
             // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 87);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Y (&&)";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnY.Location = new System.Drawing.Point(3, 3);
+            this.btnY.Name = "btnY";
+            this.btnY.Size = new System.Drawing.Size(90, 87);
+            this.btnY.TabIndex = 0;
+            this.btnY.Text = "Y (&&)";
+            this.btnY.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnO
             // 
-            this.button2.Location = new System.Drawing.Point(99, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 87);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "O (||)";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnO.Location = new System.Drawing.Point(99, 3);
+            this.btnO.Name = "btnO";
+            this.btnO.Size = new System.Drawing.Size(90, 87);
+            this.btnO.TabIndex = 1;
+            this.btnO.Text = "O (||)";
+            this.btnO.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnNo
             // 
-            this.button3.Location = new System.Drawing.Point(195, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 87);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "NO (!)";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnNo.Location = new System.Drawing.Point(195, 3);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(90, 87);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "NO (!)";
+            this.btnNo.UseVisualStyleBackColor = true;
             // 
             // frmOperadoresLogicos
             // 
@@ -145,9 +145,9 @@ namespace OperadoresLogicos
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.lblresultado);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -166,12 +166,12 @@ namespace OperadoresLogicos
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnY;
+        private System.Windows.Forms.Button btnO;
+        private System.Windows.Forms.Button btnNo;
     }
 }
