83ed79cb9b33f194e7a7f5cc13cee892643010e7
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-20 22:37:33 -0500

    Agregamos el proyecto

---
+++ frmOperadoresLogicos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OperadoresLogicos
+{
+    public partial class frmOperadoresLogicos : Form
+    {
+        public frmOperadoresLogicos()
+        {
+            InitializeComponent();
+        }
+    }
+}
