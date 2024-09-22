e368f5ae78308a309f59f03e2dd2fa33e5c5e69f
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-21 20:21:29 -0500

    Mejoramos la interfaz de usuario agregando una imagen

--- frmOperadoresLogicos.Designer.cs
+++ frmOperadoresLogicos.Designer.cs
@@ -40,113 +40,179 @@ namespace OperadoresLogicos
             this.btnY = new System.Windows.Forms.Button();
             this.btnO = new System.Windows.Forms.Button();
             this.btnNo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(312, 56);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Broadway", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.Size = new System.Drawing.Size(1282, 198);
             this.label1.TabIndex = 0;
             this.label1.Text = "OPERADORES LOGICOS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 113);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(372, 201);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.Size = new System.Drawing.Size(336, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ingresamos el Número 1:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 160);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(372, 282);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.Size = new System.Drawing.Size(336, 32);
             this.label3.TabIndex = 1;
             this.label3.Text = "Ingresamos el Número 2:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(169, 333);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(506, 615);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.Size = new System.Drawing.Size(185, 40);
             this.label4.TabIndex = 2;
             this.label4.Text = "Resultado:";
             // 
             // lblresultado
             // 
             this.lblresultado.AutoSize = true;
-            this.lblresultado.Location = new System.Drawing.Point(255, 333);
+            this.lblresultado.BackColor = System.Drawing.Color.Transparent;
+            this.lblresultado.Font = new System.Drawing.Font("Gadugi", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.Lime;
+            this.lblresultado.Location = new System.Drawing.Point(763, 615);
             this.lblresultado.Name = "lblresultado";
-            this.lblresultado.Size = new System.Drawing.Size(16, 13);
+            this.lblresultado.Size = new System.Drawing.Size(65, 45);
             this.lblresultado.TabIndex = 3;
             this.lblresultado.Text = "---";
             // 
             // txtnumero1
             // 
-            this.txtnumero1.Location = new System.Drawing.Point(264, 113);
+            this.txtnumero1.BackColor = System.Drawing.Color.Fuchsia;
+            this.txtnumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero1.ForeColor = System.Drawing.Color.White;
+            this.txtnumero1.Location = new System.Drawing.Point(787, 201);
             this.txtnumero1.Name = "txtnumero1";
-            this.txtnumero1.Size = new System.Drawing.Size(172, 20);
+            this.txtnumero1.Size = new System.Drawing.Size(218, 35);
             this.txtnumero1.TabIndex = 4;
+            this.txtnumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtnumero2
             // 
-            this.txtnumero2.Location = new System.Drawing.Point(264, 153);
+            this.txtnumero2.BackColor = System.Drawing.Color.Fuchsia;
+            this.txtnumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero2.ForeColor = System.Drawing.Color.White;
+            this.txtnumero2.Location = new System.Drawing.Point(787, 279);
             this.txtnumero2.Name = "txtnumero2";
-            this.txtnumero2.Size = new System.Drawing.Size(172, 20);
+            this.txtnumero2.Size = new System.Drawing.Size(218, 35);
             this.txtnumero2.TabIndex = 4;
+            this.txtnumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.flowLayoutPanel1.Controls.Add(this.btnY);
             this.flowLayoutPanel1.Controls.Add(this.btnO);
             this.flowLayoutPanel1.Controls.Add(this.btnNo);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(255, 199);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(496, 381);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(304, 90);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(379, 127);
             this.flowLayoutPanel1.TabIndex = 5;
             // 
             // btnY
             // 
+            this.btnY.BackColor = System.Drawing.Color.Brown;
+            this.btnY.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnY.FlatAppearance.BorderSize = 2;
+            this.btnY.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnY.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnY.ForeColor = System.Drawing.Color.Black;
             this.btnY.Location = new System.Drawing.Point(3, 3);
             this.btnY.Name = "btnY";
-            this.btnY.Size = new System.Drawing.Size(90, 87);
+            this.btnY.Size = new System.Drawing.Size(114, 113);
             this.btnY.TabIndex = 0;
             this.btnY.Text = "Y (&&)";
-            this.btnY.UseVisualStyleBackColor = true;
+            this.btnY.UseVisualStyleBackColor = false;
             this.btnY.Click += new System.EventHandler(this.btnY_Click);
             // 
             // btnO
             // 
-            this.btnO.Location = new System.Drawing.Point(99, 3);
+            this.btnO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnO.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnO.FlatAppearance.BorderSize = 2;
+            this.btnO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnO.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnO.ForeColor = System.Drawing.Color.Black;
+            this.btnO.Location = new System.Drawing.Point(123, 3);
             this.btnO.Name = "btnO";
-            this.btnO.Size = new System.Drawing.Size(90, 87);
+            this.btnO.Size = new System.Drawing.Size(119, 113);
             this.btnO.TabIndex = 1;
             this.btnO.Text = "O (||)";
-            this.btnO.UseVisualStyleBackColor = true;
+            this.btnO.UseVisualStyleBackColor = false;
             this.btnO.Click += new System.EventHandler(this.btnO_Click);
             // 
             // btnNo
             // 
-            this.btnNo.Location = new System.Drawing.Point(195, 3);
+            this.btnNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNo.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnNo.FlatAppearance.BorderSize = 2;
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNo.ForeColor = System.Drawing.Color.Black;
+            this.btnNo.Location = new System.Drawing.Point(248, 3);
             this.btnNo.Name = "btnNo";
-            this.btnNo.Size = new System.Drawing.Size(90, 87);
+            this.btnNo.Size = new System.Drawing.Size(120, 113);
             this.btnNo.TabIndex = 2;
             this.btnNo.Text = "NO (!)";
-            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.UseVisualStyleBackColor = false;
             this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(336, 558);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 10);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Location = new System.Drawing.Point(744, 558);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 10);
+            this.panel2.TabIndex = 6;
             // 
             // frmOperadoresLogicos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = global::OperadoresLogicos.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1282, 881);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.txtnumero2);
             this.Controls.Add(this.txtnumero1);
@@ -155,8 +221,11 @@ namespace OperadoresLogicos
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "frmOperadoresLogicos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmOperadoresLogicos";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmOperadoresLogicos_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -177,5 +246,7 @@ namespace OperadoresLogicos
         private System.Windows.Forms.Button btnY;
         private System.Windows.Forms.Button btnO;
         private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
